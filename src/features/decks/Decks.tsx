e2cbdf08080aebdbed94ce32baf884719fbc586d
«Elena <LenaRybinskova@gmail.com>
2024-04-04 16:28:05 +0300

    1: HW1 decksAPI, типизация

--- src/features/decks/Decks.tsx
+++ src/features/decks/Decks.tsx
@@ -1,7 +1,14 @@
 import { DecksList } from './DecksList/DecksList.tsx'
 import { AddNewDeckForm } from './AddNewDeckForm/AddNewDeckForm.tsx'
+import {useEffect} from 'react';
+import {decksAPI} from './decks-api';
 
 export const Decks = () => {
+    console.log("Decks")
+    useEffect(()=>{
+        decksAPI.fetchDecks().then(res => console.log(res.data.items))
+    },[])
+
   return (
     <div>
       <h1>Decks 🦝</h1>
--- src/features/decks/decks-api.ts
+++ src/features/decks/decks-api.ts
@@ -1,8 +1,50 @@
 import axios from 'axios'
 
 export const instance = axios.create({
-  baseURL: 'https://api.flashcards.andrii.es',
-  headers: {
-    'x-auth-skip': true,
-  },
+    baseURL: 'https://api.flashcards.andrii.es',
+    headers: {
+        'x-auth-skip': true,
+    },
 })
+
+
+export const decksAPI = {
+    fetchDecks() {
+        return instance.get<FetchResponseType>('/v2/decks')
+    }
+}
+
+export type ErrorResponseType = {
+    statusCode: number,
+    message: string,
+    timestamp: string,
+    path: string
+}
+
+export type FetchResponseType = {
+    items: DeckType[],
+    pagination: PaginationType
+}
+
+export type AuthorType={
+    id: string,
+    name: string
+}
+
+export type DeckType ={
+    author: AuthorType,
+    id: string,
+    userId: string,
+    name: string,
+    isPrivate: boolean,
+    cover: string,
+    created: string,
+    updated: string,
+    cardsCount: number
+}
+export type PaginationType={
+    currentPage: number,
+    itemsPerPage: number,
+    totalPages: number,
+    totalItems: number
+}
